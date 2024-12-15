dd6c957fdb8e87d87bb8f7cd41e1de7428792408
unknown <mostafa.khalifa.2000.mk@gmail.com>
2024-12-15 10:23:56 +0200

    edit file resource

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,21 +1,24 @@
-// amplify/auth/resource.ts
 import { defineAuth } from "@aws-amplify/backend";
 
 export const auth = defineAuth({
   loginWith: {
     email: true,
   },
-  authenticatedRole: {
-    statements: [
-      {
-        effect: "Allow",
-        actions: [
-          "sns:Publish",
-          "sns:Subscribe",
-          "sns:ListSubscriptionsByTopic",
+  access: {
+    authenticatedRole: {
+      iam: {
+        statements: [
+          {
+            effect: "Allow",
+            actions: [
+              "sns:Publish",
+              "sns:Subscribe",
+              "sns:ListSubscriptionsByTopic",
+            ],
+            resources: ["arn:aws:sns:us-east-1:911167894190:loginMessage"],
+          },
         ],
-        resources: ["arn:aws:sns:us-east-1:911167894190:loginMessage"],
       },
-    ],
+    },
   },
 });
